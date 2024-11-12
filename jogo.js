19a267da422ffb87d83979c9e333bfde2c76fdd6
guizero41 <161616495+guizero41@users.noreply.github.com>
2024-11-12 15:47:02 -0300

    Add files via upload

--- jogo.js
+++ jogo.js
@@ -76,6 +76,40 @@ let cardsChosen = []; //cartas escolhidas
 let cardsChosenId = [];//id das cartas escolhidas
 let cardsWon = [] //cartas combinas
 
+//checagem de combinações 
+function checkForMatch(){
+   const cards = document.querySelectorAll('img');
+   const option0oneId = cardsChosenId[0];
+   const optionTwoId = cardsChosenId[1];
+
+   //verificar clique na mesma imagem
+   if(option0oneId == optionTwoId){
+      cards[option0oneId].setAttribute('src','images/imagem.png');
+      cards[option0oneId].setAttribute('src','images/imagem.png');
+      alert('você clicou na mesma imagem');
+   }
+   //verificar combinação se click em imagens diferentes
+   else if(cardsChosen[0] === cardsChosen[1]){
+      cards[option0oneId].setAttribute('src', 'images/check.png');
+      cards[option0oneId].setAttribute('src', 'images/check.png');
+      cards[option0oneId].removeEventListener('click',flipCard);
+      cards[option0oneId].removeEventListener('click',flipCard);
+      cardsWon.push(cardsChosen);
+      alert('você encontrou uma combinação')
+}else{
+   cards[option0oneId].setAttribute('src', 'images/imagens.png');
+   cards[option0oneId].setAttribute('src', 'images/imagens.png');
+   alert('errou, tente novamente');
+}
+   cardsChosen = [];
+   cardsChosenId = [];
+//mostrar placar
+resultView.textContent = 'pares encontrados: '+cardsWon.length
+if(cards.Won.length === cards.length/2){
+   resultView.textContent = 'parabéns, você ganhou'; 
+}
+
+}
 //criar o quadro de cartas
 function createBoard(){
    for(let i = 0; i < cards.length; i++){
@@ -92,6 +126,9 @@ function createBoard(){
       cardsChosen.push(cards[cardId.name]);
       cardsChosenId.push(cardId);
       this.setAttribute('src', cards[cardId].img);
+      if(cardsChosen.length === 2){
+         setTimeout(checkForMatch(), 500);
+      }
    }
    createBoard();
 });
